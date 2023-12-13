ddf7b11934f6a19e7147fa17604dafc17dac6c50
Balmer <yannick.balmer@habitat-reflechi.com>
2023-12-13 10:22:47 +0100

    Update index.js
    Erase password

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const mongoose= require('mongoose')
 
 mongoose.set('strictQuery', false);
-exports.clientPromise = mongoose.connect('mongodb+srv://Yannick:motdepassemongodb@cluster0.5qdlx6o.mongodb.net/?retryWrites=true&w=majority',{useNewUrlParser: true,useUnifiedTopology: true,})
+exports.clientPromise = mongoose.connect('mongodb+srv://Yannick:motdepasse@cluster0.5qdlx6o.mongodb.net/?retryWrites=true&w=majority',{useNewUrlParser: true,useUnifiedTopology: true,})
                                 .then((m) => { console.log("connexion ok!");return m;})
                                 .catch((err) => console.log(err));
 
